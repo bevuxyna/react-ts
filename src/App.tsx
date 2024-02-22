6eb5dd9e6e017a7b9c148edc891a984532e2f724
vmelnikova <vmelnikova@phoenixit.ru>
2024-02-22 14:29:32 +0300

    Getting User Input with "refs"

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
 
 function App() {
     const todoList = [{id: 't1', text: 'Finish the course'}]
     return (
         <div className="App">
+            <NewTodo />
             <TodoList items={todoList} />
         </div>
     );
